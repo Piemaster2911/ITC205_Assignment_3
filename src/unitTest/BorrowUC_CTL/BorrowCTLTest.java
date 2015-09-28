233f62da4cf9da17f88a1da1d181e953957c4171
Piemaster2911 <norb1992@hotmail.com>
2015-09-29 06:53:19 +1000

    Creation of test for borrowCTL
    test class created. Not filled with test and mocks yet as of this time
    however.

---
+++ src/unitTest/BorrowUC_CTL/BorrowCTLTest.java
@@ -0,0 +1,52 @@
+package unitTest.BorrowUC_CTL;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class BorrowCTLTest {
+    
+    
+
+    @Before
+    public void setUp() throws Exception {
+    }
+
+    @Test
+    public void testBorrowUC_CTL() {
+    }
+
+    @Test
+    public void testInitialise() {
+    }
+
+    @Test
+    public void testClose() {
+    }
+
+    @Test
+    public void testCardSwiped() {
+    }
+
+    @Test
+    public void testBookScanned() {
+    }
+
+    @Test
+    public void testCancelled() {
+    }
+
+    @Test
+    public void testScansCompleted() {
+    }
+
+    @Test
+    public void testLoansConfirmed() {
+    }
+
+    @Test
+    public void testLoansRejected() {
+    }
+
+}
